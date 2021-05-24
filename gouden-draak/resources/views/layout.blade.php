001448c58f6ffebc09c6581fb5c11f7d95870357
Tim Kaerts <t.kaerts@student.avans.nl>
2021-05-24 18:36:43 +0200

    added bootstrap and start navigation bar

--- gouden-draak/resources/views/layout.blade.php
+++ gouden-draak/resources/views/layout.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>Gouden draak</title>
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
@@ -19,8 +19,67 @@
             font-family: 'Nunito', sans-serif;
         }
     </style>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body class="antialiased">
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Menu</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Aanbiedingen</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Nieuws</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contact</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Dropdown
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#">Action</a>
+                        <a class="dropdown-item" href="#">Another action</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">Something else here</a>
+                    </div>
+                </li>
+            </ul>
+            @if (Route::has('login'))
+                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                    @auth
+                        <a href="{{ url('/home') }}" class="text-sm text-gray-700 underline">Home</a>
+                        <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 underline">Dashboard</a>
+                    @else
+                        <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Log in</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
+        </div>
+    </nav>
+
     @yield('content')
 </body>
 </html>
--- gouden-draak/resources/views/welcome.blade.php
+++ gouden-draak/resources/views/welcome.blade.php
@@ -2,27 +2,6 @@
 
 @section('content')
     <table id="main_table" style="padding:5px;width:100%;border-collapse: collapse">
-        <tr style="height:50px;background-color:red">
-            <td style="text-align:center;width:30%;color:yellow;font-size:30px">
-                <img style="vertical-align: middle;" src="{{asset('images/dragon-small.png')}}" alt="Golden Dragon" height="50px">
-                <span style="font-family:'chinese_takeawayregular'">De Gouden Draak</span>
-                <img style="vertical-align: middle;" src="{{asset('images/dragon-small-flipped.png')}}" alt="Golden Dragon" height="50px">
-            </td>
-            <td>
-                <a href="paginas/aanbiedingen.html" style="color:yellow;font-weight:bold;text-decoration: none;">
-                    <marquee behavior="scroll" direction="left">
-                        Welkom bij De Gouden Draak. Klik op deze tekst om de aanbiedingen van deze week te zien!
-                    </marquee>
-                </a>
-            </td>
-            <td style="text-align:center;width:30%;color:yellow;font-size:30px">
-                <img style="vertical-align: middle;" src="{{asset('images/dragon-small.png')}}" alt="Golden Dragon" height="50px">
-                <span style="font-family:'chinese_takeawayregular'">De Gouden Draak</span>
-                <img style="vertical-align: middle;" src="{{asset('images/dragon-small-flipped.png')}}" alt="Golden Dragon" height="50px">
-            </td>
-        </tr>
-    </table>
-    <table id="main_table" style="padding:5px;width:100%;border-collapse: collapse">
         <tr style="height:7px;background-color:red">
             <td colspan="9">
             </td>
