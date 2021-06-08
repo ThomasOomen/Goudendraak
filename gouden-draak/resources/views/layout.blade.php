7ee56ec136c6a3649b62a587b4afcd1046c296ff
Tim Kaerts <t.kaerts@student.avans.nl>
2021-06-08 14:10:11 +0200

    updated lay-out and menu

--- gouden-draak/resources/views/layout.blade.php
+++ gouden-draak/resources/views/layout.blade.php
@@ -30,65 +30,66 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body class="antialiased">
+    <div class="container">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="{{ url('/') }}">De gouden draak</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="{{ url('/') }}">De gouden draak</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="{{ url('/') }}">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ url('/menu-card') }}">Menukaart</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ url('/bargain-index') }}">Aanbiedingen</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ url('/news') }}">Nieuws</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ url('/order') }}">Bestellen in restaurant</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ url('/order-takeaway') }}">Bestellen voor afhaal</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ url('/contact') }}">Contact</a>
+                    </li>
+                </ul>
+                @if (Route::has('login'))
+                    <div class="hidden top-0 right-0 px-6 py-4 sm:block">
+                        @auth
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                {{auth()->user()->name}}
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                @can('admin-role')
+                                    <a class="dropdown-item" href="{{ url('/dashboard') }}">Dashboard</a>
+                                @endcan
+                                @can('kassa-role')
+                                    <a class="dropdown-item" href="{{ url('/order-index') }}">Bestelling overzicht</a>
+                                    <a class="dropdown-item" href="{{ url('/find-product') }}">Zoek product</a>
+                                    <a class="dropdown-item" href="{{ url('/bargain-create') }}">Aanbiedingen</a>
+                                @endcan
+                                <a class="dropdown-item" href="{{ url('/logout') }}">Log out</a>
+                            </div>
+                        @else
+                            <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Log in</a>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="{{ url('/') }}">Home <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ url('/menu-card') }}">Menukaart</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ url('/bargain-index') }}">Aanbiedingen</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ url('/news') }}">Nieuws</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ url('/contact') }}">Contact</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ url('/order') }}">Bestellen in restaurant</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ url('/order-takeaway') }}">Bestellen voor afhaal</a>
-                </li>
-            </ul>
-            @if (Route::has('login'))
-                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-                    @auth
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            {{auth()->user()->name}}
-                        </a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            @can('admin-role')
-                                <a class="dropdown-item" href="{{ url('/dashboard') }}">Dashboard</a>
-                            @endcan
-                            @can('kassa-role')
-                                <a class="dropdown-item" href="{{ url('/order-index') }}">Bestelling overzicht</a>
-                                <a class="dropdown-item" href="{{ url('/find-product') }}">Zoek product</a>
-                                <a class="dropdown-item" href="{{ url('/bargain-create') }}">Aanbiedingen</a>
-                            @endcan
-                            <a class="dropdown-item" href="{{ url('/logout') }}">Log out</a>
-                        </div>
-                    @else
-                        <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Log in</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-        </div>
-    </nav>
+                            @if (Route::has('register'))
+                                <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
+                            @endif
+                        @endauth
+                    </div>
+                @endif
+            </div>
+        </nav>
+    </div>
 
     <table class="theme-table">
         <tr class="small-row"> <td colspan="9"></td></tr>
